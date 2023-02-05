4ff9cb6f5097a5148b4bca73e188ebe6731a27b0
“dinakhaled15” <dinakhaled1999@outlook.com>
2023-02-05 16:54:25 +0300

    Initial Commit

---
+++ Crew/CrewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrewApp.swift
+//  Crew
+//
+//  Created by Dina Alhajj Ibrahim on 05/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CrewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
