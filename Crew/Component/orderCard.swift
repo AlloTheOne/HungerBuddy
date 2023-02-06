0b5523bdd47062142f13d136dab9131ac40f0418
AlloTheOne <Allo.busy@gmail.com>
2023-02-07 01:01:39 +0300

    card ui

--- Crew/Component/orderCard.swift
+++ Crew/Component/orderCard.swift
@@ -10,7 +10,63 @@ import SwiftUI
 struct orderCard: View {
     var body: some View {
         VStack {
-            Text("Mccdonald's")
+            VStack{
+                HStack{
+                    Text("McDonald's")
+                        .bold()
+                    Spacer()
+                    Text("4 min")
+                        .font(.caption)
+                    
+                }
+                .padding(.bottom)
+                HStack{
+                    Text("20 SAR")
+                        .font(.caption)
+                    Spacer()
+                    Text("6 per one")
+                        .font(.caption)
+                    
+                }
+                .padding(.bottom)
+                HStack{
+                    Text("STC Pay")
+                        .font(.caption)
+                    Spacer()
+                    Text("6.2")
+                        .font(.caption)
+                    
+                }
+                .padding(.bottom)
+                HStack{
+                    Text("Jahez")
+                        .font(.caption)
+                    Spacer()
+                    Text("3-4")
+                        .font(.caption)
+                    
+                }
+                .padding(.bottom)
+                
+                Button {
+                    
+                } label: {
+                    Text("Join")
+                        .bold()
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 40)
+                        .background(Color("ouryellow"))
+                        .cornerRadius(10)
+                    
+                }
+
+            }
+                .padding()
+                .frame(width: 250, height: 215)
+                .background(.white)
+                .cornerRadius(10)
+                .shadow(color: Color("shadow"), radius: 30, x: 0, y: 0)
         }
     }
 }
