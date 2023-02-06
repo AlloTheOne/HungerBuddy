3269bef7e0f5750228d3ad1dceaa729d792aefd7
AlloTheOne <Allo.busy@gmail.com>
2023-02-07 00:28:58 +0300

    added order card file

---
+++ Crew/Component/orderCard.swift
@@ -0,0 +1,22 @@
+//
+//  orderCard.swift
+//  Crew
+//
+//  Created by Alaa Alabdullah on 06/02/2023.
+//
+
+import SwiftUI
+
+struct orderCard: View {
+    var body: some View {
+        VStack {
+            Text("Mccdonald's")
+        }
+    }
+}
+
+struct orderCard_Previews: PreviewProvider {
+    static var previews: some View {
+        orderCard()
+    }
+}
---
+++ Crew/mainPage.swift
@@ -0,0 +1,24 @@
+//
+//  mainPage.swift
+//  Crew
+//
+//  Created by Alaa Alabdullah on 06/02/2023.
+//
+
+import SwiftUI
+
+struct mainPage: View {
+    var body: some View {
+        VStack {
+        headerView()
+        
+            
+        }
+    }
+}
+
+struct mainPage_Previews: PreviewProvider {
+    static var previews: some View {
+        mainPage()
+    }
+}
