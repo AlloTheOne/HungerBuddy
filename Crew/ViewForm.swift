a975b7ae64188706e97303929b14e4d35d060eaa
“dinakhaled15” <dinakhaled1999@outlook.com>
2023-02-07 09:53:53 +0300

    update

--- Crew/ViewForm.swift
+++ Crew/ViewForm.swift
@@ -7,13 +7,26 @@
 import SwiftUI
 
 struct ViewForm: View {
+    @State var cityName: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("New Order")
+                .foregroundColor(.white)
+                .font(.title3)
+                .bold()
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.black.ignoresSafeArea(edges: .top))
+                .frame(maxWidth: .infinity)
+            Spacer()
+            
+        }
+       
     }
-}
-
-struct ViewForm_Previews: PreviewProvider {
-    static var previews: some View {
-        ViewForm()
+    
+    struct ViewForm_Previews: PreviewProvider {
+        static var previews: some View {
+            ViewForm()
+        }
     }
 }
