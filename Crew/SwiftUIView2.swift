5af98246f059c2dbd2e7ad7ce943414bc1a45650
“dinakhaled15” <dinakhaled1999@outlook.com>
2023-02-08 18:12:08 +0300

    form done

---
+++ Crew/SwiftUIView2.swift
@@ -0,0 +1,40 @@
+//
+//  SwiftUIView2.swift
+//  Crew
+//
+//  Created by Dina Alhajj Ibrahim on 08/02/2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView2: View {
+    @State var cityName: String = ""
+    @State private var checked = true
+    var body: some View {
+        
+       
+            
+            VStack {
+                Text("How Can People Pay You?")
+                    .font(.headline)
+                TextField("", text: $cityName, prompt: Text("Payment..."))
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 15)
+                            .stroke(.gray)
+                    )
+                    .frame(maxWidth: .infinity)
+                
+                
+            }
+        }
+        
+    
+    
+        struct SwiftUIView2_Previews: PreviewProvider {
+            static var previews: some View {
+                SwiftUIView2()
+            }
+        }
+    }
+
--- Crew/mainPage.swift
+++ Crew/mainPage.swift
@@ -2,23 +2,90 @@
 //  mainPage.swift
 //  Crew
 //
-//  Created by Alaa Alabdullah on 06/02/2023.
+//  Created by Dina Alhajj Ibrahim on 06/02/2023.
 //
 
 import SwiftUI
 
 struct mainPage: View {
+    @State var cityName: String = ""
     var body: some View {
-        VStack {
-        headerView()
-        
+        VStack(spacing: 0){
+            VStack {
+                HStack { Spacer() }
+                Spacer()
+                HStack {
+                    Text("New Order")
+                    
+                        .font(.title)
+                        .bold()
+                   // Spacer()
+                    
+                }
+            }
+            .frame(height: 50)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color("darkgrey"))
+       // .ignoresSafeArea()
             
+         
+                TextField("", text: $cityName, prompt: Text("Restaurant Name"))
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 15)
+                          .stroke(.gray))
+                    .frame(height: 50)
+                    .padding()
+                ViewForm()
+                .padding()
+           // Spacer()
+               // .padding()
+            
+            TextField("", text: $cityName, prompt: Text("Delivery Fee"))
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 15)
+                      .stroke(.gray))
+                .frame(height: 50)
+                .padding()
+            SwiftUIView5()
+            .padding()
+            //Spacer()
+            TextField("", text: $cityName, prompt: Text("Add Notes and comments"))
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 15)
+                      .stroke(.gray))
+                .frame(height: 50)
+                .padding()
+            Spacer()
+            Button {
+
+            } label: {
+                Text("Send")
+                    .bold()
+                    .foregroundColor(.white)
+                    .frame(width: 250, height: 250)
+                    .frame(height: 40)
+                    .background(Color("ouryellow"))
+                    .cornerRadius(10)
+                    
+            }
+            Spacer()
+                .padding()
+                }
+        
+              }
+    
+        }
+
+    
+    
+    
+    struct mainPage_Previews: PreviewProvider {
+        static var previews: some View {
+            mainPage()
         }
     }
-}
 
-struct mainPage_Previews: PreviewProvider {
-    static var previews: some View {
-        mainPage()
-    }
-}
