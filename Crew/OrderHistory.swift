ba4fb01da5c9de0952ebfbc76e787736e5d98d02
Abeer Altalhi <abeerabdrabu17@gmail.com>
2023-02-12 09:57:24 +0300

    First Update OrderHistory Page

--- Crew/OrderHistory.swift
+++ Crew/OrderHistory.swift
@@ -9,9 +9,92 @@ import SwiftUI
 
 struct OrderHistory: View {
     var body: some View {
-        Text("Order History")
+        
+        VStack {
+            VStack {
+                HStack { Spacer() }
+                Spacer()
+                HStack {
+                    Text("Order History")
+                    
+                        .font(.title)
+                        .bold()
+                    //Spacer()
+                    
+                }
+            }
+            .frame(height: 50)
+            .foregroundColor(.white)
+            .padding(.vertical)
+            .background(Color("darkgrey"))
+            .offset(x: 0, y: -100)
+            VStack{
+                HStack{
+                    Text("McDonald's")
+                        .padding(.leading)
+                    Spacer()
+                    Text("08:43")
+                        .padding(.trailing, 10.0)
+                    
+                }
+                VStack{
+                    Text("Done")
+                        .foregroundColor(Color.green)
+                        .foregroundColor(Color.gray)
+                        .padding(.trailing, 300.0)
+                }
+                Divider()
+    
+                VStack{
+                    HStack{
+                        Text("KFC")
+                            .padding(.leading)
+                        Spacer()
+                        Text("Sun")
+                            .padding(.trailing, 10.0)
+                        
+                    }
+                    VStack{
+                        Text("Done")
+                            .foregroundColor(Color.green)
+                            .foregroundColor(Color.gray)
+                            .padding(.trailing, 300.0)
+                    }
+                    Divider()
+                }
+                VStack{
+                    HStack{
+                        Text("Burger king")
+                            .padding(.leading)
+                        Spacer()
+                        Text("23 Mar")
+                            .padding(.trailing, 10.0)
+                        
+                    }
+                    VStack{
+                        Text("Done")
+                            .foregroundColor(Color.green)
+                            .foregroundColor(Color.gray)
+                            .padding(.trailing, 300.0)
+                    }
+                    Divider()
+                }
+                
+                
+            }
+            .offset(x: 0, y: -90)
+        }
+                    .padding(.bottom, 300.0)
+
+                    
+                    
+
+
+                
+            
+        }
     }
-}
+
 
 struct OrderHistory_Previews: PreviewProvider {
     static var previews: some View {
